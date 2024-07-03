69f1039f86cca9d6b833f0a19b3352cf40dcf602
benbihi <elm.benbihi@gmail.com>
2024-07-03 16:23:08 -0700

    task 4 ssh 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 # puppet script
 
-file {
-ensure  => present,
-content => "
-#ssh config file
-host*
-IdentityFile ~/.ssh/school
-PasswordAuthentication no
-"
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
